c7716b2c9a79cda2ce4e305c190466973b04dcce
CliveAddai <44880447+CliveAddai@users.noreply.github.com>
2021-10-09 14:37:27 -0400

    Add files via upload

---
+++ Scripts/Enemy_Wander.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Wander : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float range = 2;
+    public float maxDistance = 5;
+
+    Vector2 wayPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetNewDestination();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, wayPoint, moveSpeed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, wayPoint) < range)
+        {
+            SetNewDestination();
+        }
+
+    }
+
+    void SetNewDestination()
+    {
+        wayPoint = new Vector2(Random.Range(-maxDistance, maxDistance), Random.Range(-maxDistance, maxDistance));
+    }
+
+}
